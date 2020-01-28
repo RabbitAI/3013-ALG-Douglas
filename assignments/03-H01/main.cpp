6e69a3f83ca80256dab27f4cb430fd722e175a37
Christian Douglas <cedoug13@aol.com>
2020-01-28 11:05:28 -0600

    added Griffin's code to main.cpp

--- assignments/03-H01/main.cpp
+++ assignments/03-H01/main.cpp
@@ -1,320 +1,242 @@
-/******************************************************************************
- * 
- * Author:            Christian Douglas
- * Email:             cedoug13@aol.com
- * Label:             A02
- * Title:             02-CommentedCode
- * Course:            CMPS 3013
- * Semester:          Spring 2020
- * 
- * Description: 
- *       The program creates a linked list that operates like an array. It
- *       overloads the square brackets to simulate the appearance of accessing
- *       an array, but it actually traverses the list using an index value to 
- *       find a specfic node. The plus sign is also overloaded to be able to 
- *       push new nodes onto the end of the list.  
- *              
- * Usage:
- *      -$ ./main filename
- *      - reads into our list the values from a file. 
- * Files:     
- *      main.cpp     : driver program
- *****************************************************************************/
+///////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Terry Griffin
+// Email:            terry.griffin@msutexas.edu
+// Label:            L01
+// Title:            Lecture 01 - Array Based Stack
+// Course:           3013
+// Semester:         Spring 2020
+//
+// Description:
+//       Overview of a class implementing an array based stack
+//
+/////////////////////////////////////////////////////////////////////////////////
 #include <iostream>
 
 using namespace std;
 
-int A[100];         //Global array with size of 100
-
 /**
- * Node
+ * ArrayStack
  * 
  * Description:
- *       Allows a node to be created for a linked list
+ *      Array based stack
  * 
  * Public Methods:
- *                  Node()
- *                  Node(int n)   
- */
-struct Node {
-    int x;
-    Node *next;
-    Node() {
-        x = -1;
-        next = NULL;
-    }
-    Node(int n) {
-        x = n;
-        next = NULL;
-    }
-};
-
-/**
- * List
+ *      - See class below
  * 
- * Description: 
- *        The class creates a linked list that acts like an array.
- * 
- * Private Methods:
- *      none
- * Public Methods:
- *                  List()
- *      void        Push(int val)
- *      void        Insert(int val)
- *      void        PrintTail()
- *      string      Print()
- *      int         Pop()
- *      List        operator+(const List &Rhs)
- *      int         operator[](int index)
- *      ostream     &operator<<(ostream &os, List L)
+ * Usage: 
+ *      - See main program
+ *      
  */
-class List {
+class ArrayStack{
 private:
-    Node *Head;         //Head pointer for beginning list
-    Node *Tail;         //Tail pointer for end of list
-    int Size;           //Holds the number of nodes in the list
+  int *A;           // pointer to array of int's
+  int size;         // current max stack size
+  int top;          // top of stack 
 
 public:
-    /**
-     * Public : List
-     * 
-     * Description:
-     *      default constructor
-     * 
-     * Params: 
-     *       none
-     * Returns:
-     *        void  :   returns nothing
-     */
-    List() {
-        Head = Tail = NULL;
-        Size = 0;
+ /**
+  * ArrayStack
+  * 
+  * Description:
+  *      Constructor no params
+  * 
+  * Params:
+  *     - None
+  * 
+  * Returns:
+  *     - NULL
+  */
+  ArrayStack(){
+    size = 10;
+    A = new int[size];
+    top = -1;
+  }
+
+ /**
+  * ArrayStack
+  * 
+  * Description:
+  *      Constructor size param
+  * 
+  * Params:
+  *     - int size
+  * 
+  * Returns:
+  *     - NULL
+  */
+  ArrayStack(int s){
+    size = s;
+    A = new int[s];
+    top = -1;
+  }
+
+ /**
+  * Public bool: Empty
+  * 
+  * Description:
+  *      Stack empty?
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      [bool] true = empty
+  */
+  bool Empty(){
+    return (top <= -1);
+  }
+  
+ /**
+  * Public bool: Full
+  * 
+  * Description:
+  *      Stack full?
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      [bool] true = full
+  */
+  bool Full(){
+    return (top >= size-1);
+  }
+
+ /**
+  * Public int: Peek
+  * 
+  * Description:
+  *      Returns top value without altering the stack
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      [int] top value if any
+  */
+  int Peek(){
+    if(!Empty()){
+      return A[top];
+    }
+    
+    return -99; // some sentinel value
+                // not a good solution
+  }
+
+ /**
+  * Public int: Pop
+  * 
+  * Description:
+  *      Returns top value and removes it from stack
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      [int] top value if any
+  */
+  int Pop(){
+    if(!Empty()){
+      return A[top--];
     }
 
-    /**
-     * Public : Push
-     * 
-     * Description:
-     *      Adds a new node to the end of a list
-     * 
-     * Params: 
-     *       int    :   val
-     * Returns:
-     *        void  :   returns nothing
-     */
-    void Push(int val) {
-        // allocate new memory and init node
-        Node *Temp = new Node(val);
+    return -99; // some sentinel value
+                // not a good solution
+  }
 
-        if (!Head && !Tail) {
-            Head = Tail = Temp;
-        } else {
-            Tail->next = Temp;
-            Tail = Temp;
-        }
-        Size++;
+ /**
+  * Public void: Print
+  * 
+  * Description:
+  *      Prints stack to standard out
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      NULL
+  */
+  void Print(){
+    for(int i=0;i<=top;i++){
+      cout<<A[i]<<" ";
+    }
+    cout<<endl;
+  } 
+
+ /**
+  * Public bool: Push
+  * 
+  * Description:
+  *      Adds an item to top of stack
+  * 
+  * Params:
+  *      [int] : item to be added
+  * 
+  * Returns:
+  *      [bool] ; success = true
+  */
+  bool Push(int x){
+    if(Full()){
+      Resize();
+    }
+    if(!Full()){
+      A[++top] = x;
+      return true;
+    }
+    
+    return false;
+    
+  }
+
+ /**
+  * Public void: Resize
+  * 
+  * Description:
+  *      Resizes the container for the stack by doubling
+  *      its capacity
+  * 
+  * Params:
+  *      NULL
+  * 
+  * Returns:
+  *      NULL
+  */
+  void Resize(){
+    int newSize = size*2;       // double size of original
+    int *B = new int[newSize];  // allocate new memory
+
+    for(int i=0;i<size;i++){    // copy values to new array
+      B[i] = A[i];
     }
 
-    /**
-     * Public : Insert
-     * 
-     * Description:
-     *      Inserts a node
-     * 
-     * Params: 
-     *       int    :   val
-     * Returns:
-     *        void  :   returns nothing
-     */
-    void Insert(int val) {
-        // allocate new memory and init node
-        Node *Temp = new Node(val);
+    delete [] A;                // delete old array
 
-        // figure out where it goes in the list
+    size = newSize;             // save new size
 
-        Temp->next = Head;
-        Head = Temp;
-        if (!Tail) {
-            Tail = Head;
-        }
-        Size++;
-    }
+    A = B;                      // reset array pointer
 
-    /**
-     * Public : PrintTail
-     * 
-     * Description:
-     *      Prints the node at the end of the list
-     * 
-     * Params: 
-     *       none
-     * Returns:
-     *        void  :   returns nothing
-     */
-    void PrintTail() {
-        cout << Tail->x << endl;
-    }
+  }
 
-    /**
-     * Public : Print
-     * 
-     * Description:
-     *      Prints the entire list
-     * 
-     * Params: 
-     *       none
-     * Returns:
-     *        string    :   returns each node as a string
-     */
-    string Print() {
-        Node *Temp = Head;
-        string list;
-
-        while (Temp != NULL) {
-            list += to_string(Temp->x) + "->";
-            Temp = Temp->next;
-        }
-
-        return list;
-    }
-
-    /**
-     * Public : Pop
-     * 
-     * Description:
-     *      decrements the Size variable
-     * 
-     * Params: 
-     *       none
-     * Returns:
-     *        int   :   returns 0
-     */
-    // not implemented
-    int Pop() {
-        Size--;
-        return 0; //
-    }
-
-    /**
-     * Public : operator+
-     * 
-     * Description:
-     *      overloads the plus sign to push a value on the list
-     * 
-     * Params: 
-     *       const List    :    List object
-     * Returns:
-     *        List         :    allows the plus sign to be overloaded
-     */
-    List operator+(const List &Rhs) {
-        // Create a new list that will contain both when done
-        List NewList;
-
-        // Get a reference to beginning of local list
-        Node *Temp = Head;
-
-        // Loop through local list and Push values onto new list
-        while (Temp != NULL) {
-            NewList.Push(Temp->x);
-            Temp = Temp->next;
-        }
-
-        // Get a reference to head of Rhs
-        Temp = Rhs.Head;
-
-        // Same as above, loop and push
-        while (Temp != NULL) {
-            NewList.Push(Temp->x);
-            Temp = Temp->next;
-        }
-
-        // Return new concatenated version of lists
-        return NewList;
-    }
-
-    /**
-     * Public : operator[]
-     * 
-     * Description:
-     *      overloads the square brackets to have the list simulate an array
-     * 
-     * Params: 
-     *       int    :   indexes each node
-     * Returns:
-     *       int    :   allows the list to simulate an array and index each
-     *                  node
-     */
-    // Implementation of [] operator.  This function returns an
-    // int value as if the list were an array.
-    int operator[](int index) {
-        Node *Temp = Head;
-
-        if (index >= Size) {
-            cout << "Index out of bounds, exiting";
-            exit(0);
-        } else {
-
-            for (int i = 0; i < index; i++) {
-                Temp = Temp->next;
-            }
-            return Temp->x;
-        }
-    }
-
-    /**
-     * friend : operator<<
-     * 
-     * Description:
-     *      overloads the double left angle brackets to print list
-     * 
-     * Params: 
-     *       ostream    :   object of the output stream
-     *       List       :   object of List class
-     * Returns:
-     *       ostream    :   gives the List class the ability to print nodes
-     */
-    friend ostream &operator<<(ostream &os, List L) {
-        os << L.Print();
-        return os;
-    }
 };
 
-/**
- * Public : main
- * 
- * Description:
- *       main function of the program 
- * 
- * Params: 
- *       int    :   number of arguments 
- *       char** :   contains the addresses of the arguments
- * Returns:
- *        int   :   returns whether the program executes successfully
- */
-int main(int argc, char **argv) {
-    List L1;            //List's first object
-    List L2;            //List's second object
+// MAIN DRIVER
+// Simple Array Based Stack Usage:
+int main() {
+  ArrayStack stack;
+  int r = 0;
 
-    //pushes values onto L1's list
-    for (int i = 0; i < 25; i++) {
-        L1.Push(i);
+  for(int i=0;i<20;i++){
+    r = rand() % 100;
+    r = i+1;
+    if(!stack.Push(r)){
+      cout<<"Push failed"<<endl;
     }
-    //pushes values onto L2's list
-    for (int i = 50; i < 100; i++) {
-        L2.Push(i);
-    }
+  }
 
-    //cout << L1 << endl;
-    //prints the last value of L1
-    L1.PrintTail();
-    //prints the last value of L2
-    L2.PrintTail();
+  for(int i=0;i<7;i++){
+    stack.Pop();
+  }
 
-    //values the two list and puts it in a third list
-    List L3 = L1 + L2;
-    //prints L3
-    cout << L3 << endl;
-    //prints the fifth node in L3's list
-    cout << L3[5] << endl;
-    return 0;
+  stack.Print();
 }
